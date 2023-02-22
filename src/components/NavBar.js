41ee0e55fb7edc2e7efa1b458360f93662f590cb
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-02-22 12:33:44 -0700

    completed lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,19 @@
 import React from "react";
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
+  const links = [
+    { id: 1, href: "#home", link: "home"}, 
+    { id: 2, link: "about", href: "#about"}, 
+    {id: 3, link: "projects", href: "#projects"}
+  ];
+  
+  const navLinks = links.map((link) => {
+    return <a key={link.id} href={link.href}>{link.link}</a>;
+  });
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {navLinks}
+    </nav>;
 }
 
 export default NavBar;
